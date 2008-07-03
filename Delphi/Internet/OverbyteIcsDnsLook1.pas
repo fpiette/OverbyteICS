b18e9cd8ced874500b0e8fa6818181351dc24394
François Piette <francois.piette@overbyte.be>
2008-07-03 18:56:24 +0000

    Reworked for BDS2006
    git-svn-id: http://svn.overbyte.be:8443/svn/ics/trunk@49 17a045a4-f01d-714f-988e-c09671ad6f7c

--- Delphi/Internet/OverbyteIcsDnsLook1.pas
+++ Delphi/Internet/OverbyteIcsDnsLook1.pas
@@ -8,7 +8,7 @@ Version:      1.05
 EMail:        francois.piette@overbyte.be  http://www.overbyte.be
 Support:      Use the mailing list twsocket@elists.org
               Follow "support" link at http://www.overbyte.be for subscription.
-Legal issues: Copyright (C) 1997-2007 by François PIETTE
+Legal issues: Copyright (C) 1997-2008 by François PIETTE
               Rue de Grady 24, 4053 Embourg, Belgium. Fax: +32-4-365.74.56
               <francois.piette@overbyte.be>
 
@@ -52,7 +52,7 @@ interface
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, OverbyteIcsWinSock, OverbyteIcsWndControl,
-  OverbyteIcsWSocket, WSocket;
+  OverbyteIcsWSocket;
 
 const
   DNSLookVersion = 105;

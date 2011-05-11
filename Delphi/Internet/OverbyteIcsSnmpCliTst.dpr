85173052ff73a30deab62c0865f6f4882f730880
François Piette <francois.piette@overbyte.be>
2011-05-11 19:25:40 +0000

    SNMP client demo
    git-svn-id: http://svn.overbyte.be:8443/svn/ics/trunk@714 17a045a4-f01d-714f-988e-c09671ad6f7c

---
+++ Delphi/Internet/OverbyteIcsSnmpCliTst.dpr
@@ -0,0 +1,14 @@
+program OverbyteIcsSnmpCliTst;
+
+uses
+  Forms,
+  OverbyteIcsSnmpCliTst1 in 'OverbyteIcsSnmpCliTst1.pas' {SnmpClientTestForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TSnmpClientTestForm, SnmpClientTestForm);
+  Application.Run;
+end.

ef934d96a9420e57798898a91b16e2899696280a
Angus Robertson <angus@magsys.co.uk>
2018-05-30 10:50:48 +0000

    OverbyteIcsSSLEAY.pas
    Updated to support OpenSSL 1.1.1-pre7 (beta). 
    
    
    
    git-svn-id: http://svn.overbyte.be:8443/svn/ics/trunk@1362 17a045a4-f01d-714f-988e-c09671ad6f7c

--- Source/OverbyteIcsSSLEAY.pas
+++ Source/OverbyteIcsSSLEAY.pas
@@ -119,7 +119,7 @@ Nov 22, 2017  V8.51 Testing OpenSSL 1.1.1 that adds TLS/1.3
                 of macros earlier, also SSL_session_reused
               Added more constants and functions for 1.1.1, f_SSL_xx_groups
 Feb 27, 2018  V8.52 Added more EVP functions for keys, hashing and signing
-May 01, 2018  V8.54 Testing with OpenSSL 1.1.1 beta
+May 30, 2018  V8.54 Testing with OpenSSL 1.1.1 beta
 
 
 Notes - OpenSSL ssleay32 changes between 1.0.2 and 1.1.0 - August 2016
@@ -296,6 +296,8 @@ const
     OSSL_VER_1101P4= $10101004; // 1.1.1-pre4 (beta2)    { V8.53 }
     OSSL_VER_1101P5= $10101005; // 1.1.1-pre5 (beta3)    { V8.53 }
     OSSL_VER_1101P6= $10101006; // 1.1.1-pre6 (beta4)    { V8.53 }
+    OSSL_VER_1101P7= $10101007; // 1.1.1-pre7 (beta5)    { V8.54 }
+    OSSL_VER_1101P8= $10101007; // 1.1.1-pre8 (beta6)    { V8.54 }
 //  OSSL_VER_1101  = $1010100F; // 1.1.1 final           { V8.53 }
     OSSL_VER_1199  = $10101FFF; // not yet released      { V8.34 }
     OSSL_VER_MAX   = $FFFFFFFF; // maximum version       { V8.35 }
@@ -307,7 +309,7 @@ const
 
     MIN_OSSL_VER   = OSSL_VER_1002;   { V8.39 minimum is now 1.0.2 }
 //  MAX_OSSL_VER   = OSSL_VER_1100ZZ; { V8.35 1.1.0zz }
-    MAX_OSSL_VER   = OSSL_VER_1101P6; { V8.54 1.1.1-pre6 (beta4) }
+    MAX_OSSL_VER   = OSSL_VER_1101P7; { V8.54 1.1.1-pre7 (beta5) }
 
     { V8.41 PEM base64 file titles }
     PEM_STRING_HDR_BEGIN   = '-----BEGIN ';    { six hyphens }
